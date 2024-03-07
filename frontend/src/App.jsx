b0ab6347559d5593517cba48c94135162a28724a
ashishsen003 <ashishsen997@gmail.com>
2024-03-07 19:34:26 +0530

    header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Headers from './components/Headers'
 
 const App = () => {
   return (
     <BrowserRouter className='text-3xl text-red-400'>
+      <Headers />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
