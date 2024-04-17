c102d2eaefc0aacb41ee9ce88e98278bb130fe31
ashishsen003 <ashishsen997@gmail.com>
2024-04-17 20:11:22 +0530

    added about page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my blog</h1>
 
-        <p className="text-gray-500  text-xs sm:text-lg-">
+        <p className="text-gray-500 text-lg">
           Here you will find a variety of articles and tutorials and topics such
           as History, Automobile and Science etc.
         </p>
