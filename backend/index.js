c2af74243f9a806685470b95f078f62d812ec7b2
ashishsen003 <ashishsen997@gmail.com>
2024-04-18 12:26:56 +0530

    fixed pre insttallation

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
